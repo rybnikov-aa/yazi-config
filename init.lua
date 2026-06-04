75e8fcc23c1f78c980cb9a03c8724081120e618a
nexus <rybnikov.aa@yandex.ru>
2026-06-04 19:47:28 +0300

    Add yamb plugin for bookmark management with keymap integration

--- init.lua
+++ init.lua
@@ -7,3 +7,20 @@ require("git"):setup {
 	-- Order of status signs showing in the linemode
 	order = 1500,
 }
+
+-- You can configure your bookmarks by lua language
+local bookmarks = {}
+
+require("yamb"):setup {
+  -- Optional, the path ending with path seperator represents folder.
+  bookmarks = bookmarks,
+  -- Optional, recieve notification everytime you jump.
+  jump_notify = true,
+  -- Optional, the cli of fzf.
+  cli = "fzf",
+  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  -- Optional, the path of bookmarks
+  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
+        (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+}
